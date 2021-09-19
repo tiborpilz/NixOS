b4a9ce4b9ff6a805ca01950bc0e685bc92d30ac3
Tibor Pilz <tibor@pilz.berlin>
2021-09-19 21:20:30 +0200

    initial commit

---
+++ home.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ...}:
+{
+  home.packages = with pkgs; [
+    direnv
+    git
+    gnupg
+    silver-searcher
+    stow
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Tibor Pilz";
+    userEmail = "tibor@pilz.berlin";
+  };
+
+  programs.firefox.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    keyMode = "vi";
+    secureSocket = false;
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+  
+  xdg.configFile."nvim.init.vim".source = ./dotfiles/neovim/.config/nvim/init.vim;
+}
