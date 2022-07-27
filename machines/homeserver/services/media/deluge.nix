7a4757af72446454cd365923f19dc862c0a70acd
Tibor Pilz <tibor.pilz@avenga.com>
2022-07-27 23:25:49 +0200

    generate deluge.env from secrets

--- machines/homeserver/services/media/deluge.nix
+++ machines/homeserver/services/media/deluge.nix
@@ -9,6 +9,20 @@ in
     mkdir -p ${delugeConfigDir}
   '';
 
+  system.activationScripts.generateSecretEnv = stringAfter [ "var" ] ''
+    echo VPN_USER=$(cat ${config.sops.secrets.deluge_vpn_user.path}) > /tmp/deluge.env
+    echo VPN_PASS=$(cat ${config.sops.secrets.deluge_vpn_pass.path}) >> /tmp/deluge.env
+    echo PASSWORD=$(cat ${config.sops.secrets.deluge_password.path}) >> /tmp/deluge.env
+  '';
+
+  sops.secrets.deluge-env = {
+    format = "binary";
+    sopsFile = ../../secrets/deluge.env;
+  };
+  sops.secrets.deluge_vpn_user = {};
+  sops.secrets.deluge_vpn_pass = {};
+  sops.secrets.deluge_password = {};
+
   virtualisation.oci-containers.containers.deluge = {
     image = "binhex/arch-delugevpn:latest";
     ports = [
@@ -23,18 +37,19 @@ in
     ];
     environment = {
       "VPN_ENABLED" = "yes";
-      "VPN_USER" = config.sops.secrets.pia_user.path;
-      "VPN_PASS" = config.sops.secrets.pia_pass.path;
       "VPN_PROV" = "pia";
       "VPN_CLIENT" = "wireguard";
       "STRICT_PORT_FORWARD" = "yes";
       "ENABLE_PRIVOXY" = "yes";
-      "LAN_NETWORK" = "192.168.0.0/16,10.88.0.1/16";
+      "LAN_NETWORK" = "192.168.2.0/24,10.88.0.1/16";
       "NAME_SERVERS" = "1.1.1.1";
       "DELUGE_DAEMON_LOG_LEVEL" = "info";
       "DELUGE_WEB_LOG_LEVEL" = "info";
       "DEBUG" = "false";
     };
+    environmentFiles = [
+      /tmp/deluge.env
+    ];
     extraOptions = [
       "--sysctl=\"net.ipv4.conf.all.src_valid_mark=1\""
       "--privileged=true"
