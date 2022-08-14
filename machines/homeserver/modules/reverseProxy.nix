9af30d0f06bdbd62cdbc7de581425803e7a5c03f
Tibor Pilz <tibor.pilz@avenga.com>
2022-08-14 14:33:38 +0200

    add basicauth to reverse_proxy module

--- machines/homeserver/modules/reverseProxy.nix
+++ machines/homeserver/modules/reverseProxy.nix
@@ -14,8 +14,35 @@ let
     };
   };
 
-  mkProxyConfig = port: {
-    extraConfig = "reverse_proxy http://localhost:${toString port}";
+  basicAuthOptions = { ... }: {
+    options = {
+      enable = mkOption {
+        type = types.bool;
+        description = "Enable basic auth";
+        example = true;
+        default = false;
+      };
+      username = mkOption {
+        type = types.string;
+        description = "Username";
+        example = "admin";
+      };
+      password = mkOption {
+        type = types.string;
+        description = "Hashed Password";
+        example = "JDJhJDE0JGFXdjZMeTVsYnZueDZpckpQazFkSE9zN283WnZUQmc4NmQydi5rV04wYmdOZ3F0cE4zb3NP";
+      };
+    };
+  };
+
+  mkProxyConfig = port: enableauth: username: password: {
+    extraConfig = if enableauth then ''
+      reverse_proxy http://localhost:${toString port}
+      basicauth /* bcrypt {
+          ${username} ${password}
+      }
+    ''
+    else "reverse_proxy http://localhost:${toString port}";
   };
 in
 {
@@ -36,13 +63,31 @@ in
       type = types.attrsOf (types.submodule reverseProxyOptions);
       description = "Reverse Proxy";
     };
+    basicAuth = {
+      enable = mkOption {
+        default = false;
+        type = types.bool;
+        description = "Enable basic auth";
+        example = true;
+      };
+      username = mkOption {
+        type = types.str;
+        description = "Username";
+        example = "admin";
+      };
+      password = mkOption {
+        type = types.str;
+        description = "Hashed Password";
+        example = "JDJhJDE0JGFXdjZMeTVsYnZueDZpckpQazFkSE9zN283WnZUQmc4NmQydi5rV04wYmdOZ3F0cE4zb3NP";
+      };
+    };
   };
 
   config = {
     services.caddy = mkIf (cfg.proxies != { }) (mkMerge [
       {
         virtualHosts = mapAttrs'
-          (n: v: nameValuePair "${n}.${cfg.hostname}" (mkProxyConfig v.publicPort))
+          (n: v: nameValuePair "${n}.${cfg.hostname}" (mkProxyConfig v.publicPort cfg.basicAuth.enable cfg.basicAuth.username cfg.basicAuth.password))
           cfg.proxies;
         enable = true;
         email = cfg.email;
