54a9d8f59d38b4a6e528e4d9b740df2ba6da0ef0
Tibor Pilz <tibor.pilz@avenga.com>
2022-10-09 18:07:41 +0200

    Add gnupg

---
+++ home/modules/shell/gnupg.nix
@@ -0,0 +1,26 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.shell.gnupg;
+in {
+  options.modules.shell.gnupg = with types; {
+    enable   = mkBoolOpt false;
+    cacheTTL = mkOpt int 3600;  # 1hr
+  };
+
+  config = mkIf cfg.enable {
+    home.sessionVariables.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
+
+    programs.gpg.enable = true;
+
+    home.packages = [ pkgs.tomb ];
+
+    xdg.configFile."gnupg/gpg-agent.conf" = {
+      text = ''
+        default-cache-ttl ${toString cfg.cacheTTL}
+        pinentry-program ${pkgs.pinentry.gtk2}/bin/pinentry
+      '';
+    };
+  };
+}
