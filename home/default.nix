a6520a344bdea9a30f7ed417e9170da421f6ae67
Tibor Pilz <tibor.pilz@avenga.com>
2022-11-15 11:19:15 +0100

    add devenv

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-args@{ inputs, pkgs, lib, ... }:
+args@{ inputs, pkgs, lib, config, ... }:
 
 with lib;
 with lib.my;
@@ -28,4 +28,13 @@ in {
   modules.shell.direnv.enable = true;
 
   modules.editors.neovim.enable = true;
+
+  # Install MacOS applications to the user environment if the targetPlatform is Darwin
+  home.file."Applications/home-manager".source = let
+  apps = pkgs.buildEnv {
+    name = "home-manager-applications";
+    paths = config.home.packages;
+    pathsToLink = "/Applications";
+  };
+  in mkIf pkgs.stdenv.targetPlatform.isDarwin "${apps}/Applications";
 }
