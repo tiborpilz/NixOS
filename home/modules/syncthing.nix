f61c38accddfb10fc6b2b430942ad6ee65a18f9b
Tibor Pilz <tibor.pilz@avenga.com>
2022-11-19 17:05:03 +0100

    home: add syncthing darwin package

--- home/modules/syncthing.nix
+++ home/modules/syncthing.nix
@@ -6,16 +6,20 @@ let cfg = config.modules.syncthing;
 in {
   options.modules.syncthing = {
     enable = mylib.mkBoolOpt false;
+    service = mylib.mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    services.syncthing = {
+    home.packages = with pkgs; [
+      syncthing
+    ];
+    services.syncthing = mkIf cfg.service {
       enable = true;
       tray.enable = true;
     };
 
     # Need to specify tray target, manually since HM is not managing the Xsession
-    systemd.user.targets.tray = {
+    systemd.user.targets.tray = mkIf cfg.service {
       Unit = {
         Description = "Home Manager System Tray";
         Requires = [ "graphical-session-pre.target" ];
