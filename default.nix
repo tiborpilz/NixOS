7a193b91bfc184ac22349271cd430485c38dc7b5
Tibor Pilz <tibor.pilz@iu.org>
2022-11-27 12:49:30 +0100

    emacs: add nil and volar lsp

--- default.nix
+++ default.nix
@@ -12,22 +12,5 @@ with lib.my;
 
   system.configurationRevision = with inputs; mkIf (self ? rev) self.rev;
 
-    console.useXkbConfig = true;
-
-    #
-    # home-manager = import ./home-manager; # ) // {
-    # home-manager = {
-
-    #   home.username = "tibor";
-    #   home.homeDirectory = "/home/tibor";
-
-    #   home.stateVersion = "22.05";
-
-    #   # Let Home Manager install and manage itself.
-    #   programs.home-manager.enable = true;
-    #   programs.tmux.enable = true;
-    #   programs.man.enable = false;
-    # };
-    #   programs.home-manager.enable = false; # When using NixOS, I don't want to use home-manager standalone.
-    # };
+  console.useXkbConfig = true;
 }
