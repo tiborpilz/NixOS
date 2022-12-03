016de61119789d14a1830c8dc96da14b7a4e8b1e
Tibor Pilz <tibor.pilz@iu.org>
2022-12-03 21:34:54 +0100

    refactor: add homeConfigurations & fix home-manager

--- home/modules/editors/emacs.nix
+++ home/modules/editors/emacs.nix
@@ -92,11 +92,11 @@ in
 
     home.activation.installDoomEmacs = lib.hm.dag.entryAfter ["WriteBoundary"] ''
         if [ ! -d ".config/emacs" ]; then
-            git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs ".config/emacs"
+            ${pkgs.git}/bin/git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs ".config/emacs"
         fi
 
         if [ ! -d ".config/doom" ]; then
-            git clone https://github.com/tiborpilz/doom-emacs-config ".config/doom"
+            ${pkgs.git}/bin/git clone https://github.com/tiborpilz/doom-emacs-config ".config/doom"
         fi
         # .config/emacs/bin/doom sync
       '';
