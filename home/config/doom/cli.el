38d828af93a9c67a953fe3069f1e1ea71854002f
Tibor Pilz <tibor@pilz.berlin>
2022-12-04 06:09:38 +0100

    improvement: move doom config to nixos repo for performance reasons

---
+++ home/config/doom/cli.el
@@ -0,0 +1,10 @@
+;;; cli.el -*- lexical-binding: t; -*-
+(setq org-configm-babel-evaluate nil)
+
+(defun doom-shut-up-a (orign-fn &rest args)
+  (quiet! (apply origin-fn args)))
+
+(advice-add 'org-babel-execute-src-block :around #'doom-shut-up-a)
+
+;; Don't compile packages on install
+(advice-add #'native-compile-async :override #'ignore)
