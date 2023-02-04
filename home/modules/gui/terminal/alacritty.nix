22ffbf5cad3f0aa744c09236a160d613398aeb91
Tibor Pilz <tibor.pilz@iu.org>
2023-02-04 03:05:20 +0100

    not werking

---
+++ home/modules/gui/terminal/alacritty.nix
@@ -0,0 +1,30 @@
+{ inputs, config, options, pkgs, lib, ... }:
+
+let
+  cfg = config.modules.alacritty;
+  in
+with lib;
+{
+  # options.modules.gui.alacritty = {
+  #   enable = mkEnableOption "Alacritty terminal emulator";
+
+  #   config = mkOption {
+  #     type = types.attrs;
+  #     default = {};
+  #     description = "Alacritty configuration";
+  #   };
+  # };
+
+  # config = mkIf cfg.enable {
+  #   programs.alacritty = {
+  #     enable = true;
+  #   };
+
+  #   # TODO
+  #   settings = mkOption {
+  #     type = types.attrs;
+  #     default = {};
+  #     description = "Alacritty configuration";
+  #   };
+  # };
+}
