9aae85f6859584b4a23ec2dc407a9a4cacafc9da
Tibor Pilz <tibor.pilz@iu.org>
2023-07-12 13:05:14 +0200

    feat: add yubikey based ssh auth

--- home/modules/shell/gnupg.nix
+++ home/modules/shell/gnupg.nix
@@ -9,22 +9,54 @@ in
   options.modules.shell.gnupg = with types; {
     enable = mylib.mkBoolOpt false;
     cacheTTL = mylib.mkOpt int 3600; # 1hr
+    maxCacheTTL = mylib.mkOpt int 86400; # 1 day
+    keygrip = mylib.mkOpt string "Keygrip = 1050A7CD50EAFCD36E696470775BC39D6FFA47A4";
   };
 
   config = mkIf cfg.enable {
     home.sessionVariables.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
 
-    home.packages = [ pkgs.yubikey-personalization-gui ];
+    home.packages = [
+      pkgs.yubikey-personalization
+      pkgs.yubikey-manager
+      pkgs.gnupg
+    ];
 
-    programs.gpg.enable = true;
-    services.gpg-agent.enable = true;
-    services.gpg-agent.enableSshSupport = true;
+    # For some reason, `programs.gpg` doesn't work on mac, although there
+    # is a gnupg package. So I use the package directly.
+    # programs.gpg.enable = true;
+    # services.gpg-agent.enable = true;
+    # services.gpg-agent.enableSshSupport = true;
 
-    xdg.configFile."gnupg/gpg-agent.conf" = {
+    home.file.".gnupg/gpg-agent.conf" = {
       text = ''
         default-cache-ttl ${toString cfg.cacheTTL}
         pinentry-program ${pkgs.pinentry.gtk2}/bin/pinentry
+        enable-ssh-support
+        write-env-file
+        use-standard-socket
+        default-cache-ttl-ssh ${toString cfg.cacheTTL}
+        max-cache-ttl ${toString cfg.maxCacheTTL}
       '';
     };
+    home.file.".gnupg/scdaemon.conf" = {
+      text = ''
+        card-timeout 5
+        disable-ccid
+      '';
+    };
+    # SSH Key from Yubikey
+    home.file.".gnupg/sshcontrol" = {
+      text = ''
+        ${cfg.keygrip}
+      '';
+    };
+
+    # Tell SSH to use gpg-agent
+    modules.shell.zsh.rcInit = ''
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
+      echo UPDATESTARTUPTTY | gpg-connect-agent
+    '';
   };
 }
