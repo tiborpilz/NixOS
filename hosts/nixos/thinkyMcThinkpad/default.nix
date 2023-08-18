cf9effbc3b991a4467e7acc297865d60b7ba2f0b
Tibor Pilz <tibor.pilz@iu.org>
2023-08-18 09:24:49 +0200

    fixup! fix: fix nixos hosts

--- hosts/nixos/thinkyMcThinkpad/default.nix
+++ hosts/nixos/thinkyMcThinkpad/default.nix
@@ -99,6 +99,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  programs.zsh.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tibor = {
     isNormalUser = true;
