609e6c55af00877dcb68f851ad4ec186ffff7ef2
Tibor Pilz <tibor@pilz.berlin>
2023-12-19 14:40:48 +0100

    fix: package cleanup & fix gch script

--- packages/scripts/git-utils/default.nix
+++ packages/scripts/git-utils/default.nix
@@ -3,11 +3,13 @@ with pkgs;
 stdenv.mkDerivation {
   name = "git-utils";
   phases = "installPhase";
-  src = "./";
+  src = ./.;
   buildInputs = [ git fzf ];
   nativeBuildInputs = [ makeWrapper ];
   installPhase = ''
       mkdir -p $out/bin
       cp $src/gch.sh $out/bin/gch
+      wrapProgram $out/bin/gch \
+        --prefix PATH : ${lib.makeBinPath [ git fzf ]}
     '';
 }
