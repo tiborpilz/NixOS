59760a70d8ed70143b15c1d51f5dad60e426426e
Tibor Pilz <tibor@pilz.berlin>
2023-12-19 16:08:30 +0100

    nix: add garnix to cache

--- home/default.nix
+++ home/default.nix
@@ -118,11 +118,13 @@ with mylib;
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org/"
         "https://tiborpilz.cachix.org/"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "tiborpilz.cachix.org-1:KyBjAXY8eblxntQ+OG13IjT+M222VxT+25yw1lqnQS4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       system-features = [ "big-parallel" "kvm" "recursive-nix" ];
     };
