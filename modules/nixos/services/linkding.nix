00e83bf1a3942914a0522213d61a8bf0e1d16014
Tibor Pilz <tibor@pilz.berlin>
2023-12-29 05:54:46 +0100

    feat: add linkding

---
+++ modules/nixos/services/linkding.nix
@@ -0,0 +1,49 @@
+{ inputs, pkgs, lib, config, ... }:
+
+with lib;
+let
+  cfg = config.modules.services.linkding;
+  mylib = import ../../../lib { inherit inputs lib pkgs; };
+in
+with mylib;
+{
+  options.modules.services.linkding = {
+    enable = mkBoolOpt false;
+    publicPort = mkOption {
+      type = types.int;
+      default = 9090;
+    };
+    dataDir = mkOption {
+      type = types.str;
+      default = "/data/linkding";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    system.activationScripts.initLinkDing = stringAfter [ "var" ] ''
+      mkdir -p ${cfg.dataDir}/data
+    '';
+
+    virtualisation.oci-containers.containers.linkding = {
+      image = "sissbruecker/linkding:latest";
+      ports = [ "${toString cfg.publicPort}:9090" ];
+      volumes = [
+        "${cfg.dataDir}/data:/etc/linkding/data"
+      ];
+      environment = {
+        LD_CONTAINER_NAME = "linkding";
+        LD_HOST_PORT = "9090";
+        LD_HOST_DATA_DIR = "./data";
+        LD_SUPERUSER_NAME= "admin";
+        LD_SUPERUSER_PASSWORD = "changeme";
+        LD_DISABLE_BACKGROUND_TASKS = "False";
+        LD_DISABLE_URL_VALIDATION = "False";
+        LD_ENABLE_AUTH_PROXY = "False";
+      };
+    };
+
+    modules.services.reverseProxy.proxies.linkding = {
+      publicPort = cfg.publicPort;
+      auth = false;
+    };
+  };
+}
