aebf9ebfd313c32ae0577d16483b6813f0e72d82
Tibor Pilz <tibor@pilz.berlin>
2024-03-01 16:42:14 +0100

    Add klaus to hosts

---
+++ hosts/nixos/klaus/samba.nix
@@ -0,0 +1,53 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.services.samba;
+in
+{
+  services.samba-wsdd.enable = true;
+  networking.firewall.allowedTCPPorts = [ 5357 ];
+  networking.firewall.allowedUDPPorts = [ 3702 ];
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = smbnix
+      netbios name = smbnix
+      security = user
+      # use sendfile = yes
+      # max protocol = smb2
+      hosts allow = 0.0.0.0/0 192.168.2. 127.0.0.1 localhost
+      guest account = samba
+      map to guest = bad user
+    '';
+    shares = {
+      "media" = {
+        path = "/data/media";
+        browseable = "yes";
+        "guest ok" = "yes";
+        "read only" = "no";
+        "create mask" = "0777";
+        # "force user" = "smbnix";
+        # "force group" = "smbnix";
+      };
+      "downloads" = {
+        path = "/data/downloads";
+        browseable = "yes";
+        "guest ok" = "yes";
+        "read only" = "no";
+        "create mask" = "0777";
+        # "force user" = "smbnix";
+        # "force group" = "smbnix";
+      };
+    };
+  };
+  users.users.samba = {
+    uid = 1001;
+    isSystemUser = true;
+    group = "samba";
+    password = "password";
+  };
+
+  users.groups.samba = { };
+}
---
+++ hosts/nixos/klaus/wireguard.nix
@@ -0,0 +1,26 @@
+{
+  # enable NAT
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.100.0.2/24" ];
+      listenPort = 51820;
+
+      privateKeyFile = "/var/lib/wireguard/private.key";
+
+      peers = [
+        {
+          publicKey = "QzJm9puVez50UZbCUAJYZnqBdW19o1tBU0Q/WXZsbyw=";
+          allowedIPs = [ "0.0.0.0/0" ];
+
+          endpoint = "159.69.194.44:51820";
+
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
