1b34ff2beef82445de3d5afe53503279bf8e7608
Tibor Pilz <tibor.pilz@iu.org>
2024-03-06 07:59:51 +0100

    home: add cargo-make package for rust development

--- home/modules/dev/rust.nix
+++ home/modules/dev/rust.nix
@@ -12,6 +12,7 @@ in
     home.packages = with pkgs; [
       rustup # contains rustc, cargo, rustdoc, rustfmt, etc.
       cargo-watch # cargo subcommand for watching files and running commands
+      cargo-make # cargo subcommand for running tasks defined in a toml file
     ];
   };
 }
