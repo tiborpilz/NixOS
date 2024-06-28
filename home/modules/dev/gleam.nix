92742bb29a30b79a9221f38bf5cc1025d374e361
Tibor Pilz <tibor.pilz@iu.org>
2024-06-28 07:58:20 +0200

    feat: add k6 as web dev package

---
+++ home/modules/dev/gleam.nix
@@ -0,0 +1,18 @@
+{ inputs, pkgs, lib, config, ... }:
+
+with lib;
+let
+  cfg = config.modules.dev.gleam;
+  mylib = import ../../../lib { inherit inputs lib pkgs; };
+in
+with mylib;
+{
+  options.modules.dev.gleam = {
+    enable = mkBoolOpt false;
+  };
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gleam
+    ];
+  };
+}
