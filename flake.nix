bd2255e72bce1a3076ee8a747054483616b35c5f
Tibor Pilz <tibor.pilz@iu.org>
2024-06-30 19:40:55 +0200

    fix(nixos): fix nixos host nix package assignment

--- flake.nix
+++ flake.nix
@@ -60,11 +60,7 @@
 
       nixosHosts = mapModules ./hosts/nixos (hostPath: lib.my.mkHostAttrs hostPath {
         system = "x86_64-linux";
-        modules = [
-          {
-            nix.package = self.channels.x86_64-linux.nixpkgs.nix;
-          }
-        ] ++ lib.my.mapModulesRec' (toString ./modules/nixos) import;
+        modules = lib.my.mapModulesRec' (toString ./modules/nixos) import;
       });
 
       darwinHosts = mapModules ./hosts/darwin (hostPath: lib.my.mkHostAttrs hostPath {
--- hosts/nixos/thinkyMcThinkpad/default.nix
+++ hosts/nixos/thinkyMcThinkpad/default.nix
@@ -134,6 +134,7 @@
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nix.settings.system-features = [ "big-parallel" "kvm" "recursive-nix" ];
+    nix.package = pkgs.nix;
     system.stateVersion = "22.11";
   };
 }
