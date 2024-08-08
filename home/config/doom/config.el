3af8815720dce2bd430e604af8c585bd173bb277
Tibor Pilz <tibor.pilz@iu.org>
2024-08-08 09:08:25 +0200

    feat(emacs): add rudimentary mdx support

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -325,6 +325,10 @@
 
 (setq lsp-rust-features "all")
 
+(setq auto-mode-alist
+      (append '(("\\.mdx\\'" . markdown-mode))
+              auto-mode-alist))
+
 (setq company-idle-delay 0.1 ;; How long to wait before popping up
       company-minimum-prefix-length 1 ;; Show the menu after one key press
       company-tooltip-limit 10 ;; Limit on how many options to display
