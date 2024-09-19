552fcb9a37dd6caaccd9aaa27ad05313af87e17f
Tibor Pilz <tibor.pilz@iu.org>
2024-09-19 07:53:48 +0200

    feat: add Aider module for AI assistant in terminal with OpenAI API integration

---
+++ home/modules/dev/aider.nix
@@ -0,0 +1,31 @@
+# Aider module
+# Aider is an AI assistant in the terminal for editing codebases.
+# (https://github.com/paul-gauthier/aider)
+# This module installs the necessary package and sets the OpenAI API key via
+# the environment variable.
+# It expects the password to be available in `pass` via a given key.
+{ inputs, config, lib, pkgs, ... }:
+let
+  cfg = config.modules.dev.aider;
+  mylib = import ../../../lib { inherit inputs lib pkgs; };
+in
+{
+  options.modules.dev.aider = {
+    enable = mylib.mkBoolOpt false;
+    passApiKey = lib.mkOption {
+      type = lib.types.str;
+      default = "bitwarden/openai-api-key";
+      description = "The key to use to retrieve the OpenAI API key from `pass`.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages =  [
+      pkgs.unstable.aider-chat
+    ];
+
+    modules.shell.zsh.envInit = ''
+      export OPENAI_API_KEY=$(pass ${cfg.passApiKey})
+    '';
+  };
+}
