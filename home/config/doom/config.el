7323589c21492f507f0b980a2b7f8bc2ae454df3
Tibor Pilz <tibor.pilz@iu.org>
2024-12-19 14:25:28 +0100

    fix: disable lsp-booster

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -478,39 +478,8 @@ for what debugger to use. If the prefix ARG is set, prompt anyway."
 (setq lsp-signature-auto-activate nil)
 (setq lsp-signature-render-documentation nil)
 
-(setq lsp-ui-doc-max-height 60
+(setq lsp-ui-doc-max-height 400
       lsp-ui-doc-max-width 250)
-
-(defun lsp-booster--advice-json-parse (old-fn &rest args)
-  "Try to parse bytecode instead of json."
-  (or
-   (when (equal (following-char) ?#)
-     (let ((bytecode (read (current-buffer))))
-       (when (byte-code-function-p bytecode)
-         (funcall bytecode))))
-   (apply old-fn args)))
-(advice-add (if (progn (require 'json)
-                       (fboundp 'json-parse-buffer))
-                'json-parse-buffer
-              'json-read)
-            :around
-            #'lsp-booster--advice-json-parse)
-
-(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
-  "Prepend emacs-lsp-booster command to lsp CMD."
-  (let ((orig-result (funcall old-fn cmd test?)))
-    (if (and (not test?)                             ;; for check lsp-server-present?
-             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
-             lsp-use-plists
-             (not (functionp 'json-rpc-connection))  ;; native json-rpc
-             (executable-find "emacs-lsp-booster"))
-        (progn
-          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
-            (setcar orig-result command-from-exec-path))
-          (message "Using emacs-lsp-booster for %s!" orig-result)
-          (cons "emacs-lsp-booster" orig-result))
-      orig-result)))
-(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (eval-after-load 'git-timemachine
   '(progn
