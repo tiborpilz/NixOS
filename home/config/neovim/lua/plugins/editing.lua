38ac1635a321a6819736bdad1db34a68fe0c7915
Tibor Pilz <tibor.pilz@iu.org>
2024-12-30 07:46:27 +0100

    feat(nvim): cmp window improvements

--- home/config/neovim/lua/plugins/editing.lua
+++ home/config/neovim/lua/plugins/editing.lua
@@ -56,18 +56,46 @@ return {
       local cmp = require("cmp")
 
       cmp.setup({
+        view = {
+          entries = "native",
+        },
+        window = {
+          completion = cmp.config.window.bordered({
+            border = { " ", " ", " ", " ", " ", " ", " ", " " },
+          }),
+          documentation = cmp.config.window.bordered({
+            border = { " ", " ", " ", " ", " ", " ", " ", " " },
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:NormalFloat",
+          }),
+        },
+        mapping = {
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        },
         sources = {
           { name = 'nvim_lsp' },
         },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<Enter>'] = cmp.mapping.confirm({select = false}),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
+        -- formatting = {
+        --   fields = { "abbr", "kind", "menu" },
+        --   expandable_indicator = false,
+        --   format = require("lspkind").cmp_format({
+        --     mode = "symbol_text",
+        --     ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        --     show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+        --     -- The function below will be called before any actual modifications from lspkind
+        --     -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+        --     before = function (entry, vim_item)
+        --       -- ...
+        --       return vim_item
+        --     end
+        --   })
+        -- },
       })
     end,
   },
