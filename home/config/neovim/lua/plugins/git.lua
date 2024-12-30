e49d06eb8a0fe5628645321e4dab36aed0a85240
Tibor Pilz <tibor.pilz@iu.org>
2024-12-30 03:30:53 +0100

    feat(nvim): more git stuff

--- home/config/neovim/lua/plugins/git.lua
+++ home/config/neovim/lua/plugins/git.lua
@@ -1,22 +1,17 @@
-function neogit_action(popup, action, args)
-  return function()
-    require'plenary.async'.run(function()
-      require('neogit.popups.' .. popup ..'.actions')[action] {
-        get_arguments = function()
-          return args
-        end
-      }
-    end)
-  end
-end
-
 return {
+  {
+    "sindrets/diffview.nvim",
+    setup = function()
+      require("diffview").setup()
+    end,
+  },
   {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
       "isakbm/gitgraph.nvim",
+      "nvim-telescope/telescope.nvim",
     },
     opts = {
       disable_signs = false,
@@ -38,11 +33,13 @@ return {
       },
       integrations = {
         diffview = true,
+        telescope = true,
       },
     },
     keys = {
       { "<leader>gg", function() require("neogit").open() end, desc = "Open Neogit" },
       { "<leader>gl", function() require("neogit").open({ "log" }) end, desc = "Git Log" },
+      { "<leader>gb", function() require("neogit").open({ "branch" }) end, desc = "Git branch" },
     },
   },
   {
--- home/config/neovim/lua/plugins/languages.lua
+++ home/config/neovim/lua/plugins/languages.lua
@@ -241,4 +241,17 @@ return {
 
   --- Nix
   { "LnL7/vim-nix" },
+
+  --- Lua
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 }
