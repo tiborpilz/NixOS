66903273deb5990bfbb1562f3ec1bfe6b1519a11
Tibor Pilz <tibor.pilz@iu.org>
2024-12-30 09:07:05 +0100

    fix(neovim): dont override C-Space

--- home/config/neovim/lua/plugins/editing.lua
+++ home/config/neovim/lua/plugins/editing.lua
@@ -69,7 +69,6 @@ return {
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
         },
