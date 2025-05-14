8e58acbae9dc4984b102ac2392f65919efbd0bd7
Tibor Pilz <tibor.pilz@iu.org>
2025-05-14 11:05:57 +0200

    feat(paperless): move to quadlet-nix for defining podman pods

--- modules/nixos/services/paperless-ng.nix
+++ modules/nixos/services/paperless-ng.nix
@@ -2,7 +2,7 @@
 with lib;
 with lib.my;
 let
-  publicPort = 8012;
+  publicPort = 8013;
   db_user = "paperless";
   db_password = "paperless";
   db_db = "paperless";
@@ -17,66 +17,141 @@ in
     system.activationScripts.makePaperlessDir = stringAfter [ "var" ] ''
       mkdir -p /var/lib/paperless/data
       mkdir -p /data/media/paperless/{consume,export,media}
-    '';
+      '';
 
-    modules.podgroups.pods.paperless-ngx = {
-      port = "${toString publicPort}:8000";
+    virtualisation.quadlet =
+      let
+        inherit (config.virtualisation.quadlet) network pods;
+      in
+      {
+        containers = {
+          paperless-ngx-broker.containerConfig = {
+            image = "docker.io/redis:6.2";
+            volumes = [
+              "paperless-redis:/data"
+            ];
+            pod = pods.paperless-ngx-pod.ref;
+          };
 
-      containers.broker = {
-        image = "docker.io/redis:6.2";
-        volumes = [
-          "paperless-redis:/data"
-        ];
-      };
+          paperless-ngx-db.containerConfig = {
+            image = "docker.io/postgres:15";
+            volumes = [
+              "paperless-pgdata:/var/lib/postgresql/data"
+            ];
+            environments = {
+              POSTGRES_USER = db_user;
+              POSTGRES_PASSWORD = db_password;
+              POSTGRES_DB = db_db;
+            };
+            pod = pods.paperless-ngx-pod.ref;
+          };
 
-      containers.db = {
-        image = "docker.io/postgres:15";
-        volumes = [
-          "paperless-pgdata:/var/lib/postgresql/data"
-        ];
-        environment = {
-          "POSTGRES_DB" = db_db;
-          "POSTGRES_USER" = db_user;
-          "POSTGRES_PASSWORD" = db_password;
+          paperless-ngx-webserver.containerConfig = {
+            image = "ghcr.io/paperless-ngx/paperless-ngx:latest";
+            volumes = [
+              "/var/lib/paperless/data:/usr/src/paperless/data"
+              "/data/media/paperless/media:/usr/src/paperless/media"
+              "/data/media/paperless/consume:/usr/src/paperless/consume"
+              "/data/media/paperless/export:/usr/src/paperless/export"
+            ];
+            environments = {
+              PAPERLESS_AUDIT_LOG_ENABLED = "true";
+              PAPERLESS_REDIS = "redis://localhost:6379";
+              PAPERLESS_DBHOST = "localhost";
+              PAPERLESS_CONSUMPTION_DIR = "/usr/src/paperless/consume";
+              PAPERLESS_OCR_LANGUAGE = "deu";
+              PAPERLESS_TIKA_ENABLED = "1";
+              PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://localhost:3000";
+              PAPERLESS_TIKA_ENDPOINT = "http://localhost:9998";
+              DJANGO_SUPERUSER_PASSOWRD = "Password";
+              PAPERLESS_ADMIN_USER = "tibor";
+              PAPERLESS_ADMIN_PASSWORD = "changeme";
+              PAPERLESS_URL =
+                "https://paperless.${config.modules.services.reverseProxy.hostname}";
+            };
+            pod = pods.paperless-ngx-pod.ref;
+          };
+
+          paperless-ngx-gotenberg.containerConfig = {
+            image = "docker.io/thecodingmachine/gotenberg:8.11.1";
+            environments = {
+              DISABLE_GOOGLE_CHROME = "1";
+            };
+            pod = pods.paperless-ngx-pod.ref;
+          };
+
+          paperless-ngx-tika.containerConfig = {
+            image = "ghcr.io/paperless-ngx/tika:latest";
+            pod = pods.paperless-ngx-pod.ref;
+          };
+        };
+
+        pods.paperless-ngx-pod.podConfig = {
+          publishPorts = [
+            "${toString publicPort}:8000"
+          ];
         };
       };
 
-      containers.webserver = {
-        image = "ghcr.io/paperless-ngx/paperless-ngx:latest";
-        dependsOn = [ "db" "broker" ];
-        volumes = [
-          "/var/lib/paperless/data:/usr/src/paperless/data"
-          "/data/media/paperless/media:/usr/src/paperless/media"
-          "/data/media/paperless/consume:/usr/src/paperless/consume"
-          "/data/media/paperless/export:/usr/src/paperless/export"
-        ];
-        environment = {
-          "PAPERLESS_AUDIT_LOG_ENABLED" = "true";
-          "PAPERLESS_REDIS" = "redis://localhost:6379";
-          "PAPERLESS_DBHOST" = "localhost";
-          "PAPERLESS_CONSUMPTION_DIR" = "/usr/src/paperless/consume";
-          "PAPERLESS_OCR_LANGUAGE" = "deu";
-          "PAPERLESS_TIKA_ENABLED" = "1";
-          "PAPERLESS_TIKA_GOTENBERG_ENDPOINT" = "http://localhost:3000";
-          "PAPERLESS_TIKA_ENDPOINT" = "http://localhost:9998";
-          "DJANGO_SUPERUSER_PASSOWRD" = "Password";
-          "PAPERLESS_ADMIN_USER" = "tibor";
-          "PAPERLESS_ADMIN_PASSWORD" = "changeme";
-          "PAPERLESS_URL" = "https://paperless.${config.modules.services.reverseProxy.hostname}";
-        };
-      };
+    # modules.podgroups.pods.paperless-ngx = {
+    #   port = "${toString publicPort}:8000";
+    #
+    #   containers.broker = {
+    #     image = "docker.io/redis:6.2";
+    #     volumes = [
+    #       "paperless-redis:/data"
+    #     ];
+    #   };
+    #
+    #   containers.db = {
+    #     image = "docker.io/postgres:15";
+    #     volumes = [
+    #       "paperless-pgdata:/var/lib/postgresql/data"
+    #     ];
+    #     environment = {
+    #       "POSTGRES_DB" = db_db;
+    #       "POSTGRES_USER" = db_user;
+    #       "POSTGRES_PASSWORD" = db_password;
+    #     };
+    #   };
+    #
+    #   containers.webserver = {
+    #     image = "ghcr.io/paperless-ngx/paperless-ngx:latest";
+    #     dependsOn = [ "db" "broker" ];
+    #     volumes = [
+    #       "/var/lib/paperless/data:/usr/src/paperless/data"
+    #       "/data/media/paperless/media:/usr/src/paperless/media"
+    #       "/data/media/paperless/consume:/usr/src/paperless/consume"
+    #       "/data/media/paperless/export:/usr/src/paperless/export"
+    #     ];
+    #     environment = {
+    #       "PAPERLESS_AUDIT_LOG_ENABLED" = "true";
+    #       "PAPERLESS_REDIS" = "redis://localhost:6379";
+    #       "PAPERLESS_DBHOST" = "localhost";
+    #       "PAPERLESS_CONSUMPTION_DIR" = "/usr/src/paperless/consume";
+    #       "PAPERLESS_OCR_LANGUAGE" = "deu";
+    #       "PAPERLESS_TIKA_ENABLED" = "1";
+    #       "PAPERLESS_TIKA_GOTENBERG_ENDPOINT" = "http://localhost:3000";
+    #       "PAPERLESS_TIKA_ENDPOINT" = "http://localhost:9998";
+    #       "DJANGO_SUPERUSER_PASSOWRD" = "Password";
+    #       "PAPERLESS_ADMIN_USER" = "tibor";
+    #       "PAPERLESS_ADMIN_PASSWORD" = "changeme";
+    #       "PAPERLESS_URL" = "https://paperless.${config.modules.services.reverseProxy.hostname}";
+    #     };
+    #   };
+    #
+    #   containers.gotenberg = {
+    #     image = "docker.io/thecodingmachine/gotenberg:8.11.1";
+    #     environment = {
+    #       "DISABLE_GOOGLE_CHROME" = "1";
+    #     };
+    #   };
+    #
+    #   containers.tika = {
+    #     image = "ghcr.io/paperless-ngx/tika:latest";
+    #   };
+    # };
 
-      containers.gotenberg = {
-        image = "docker.io/thecodingmachine/gotenberg:8.11.1";
-        environment = {
-          "DISABLE_GOOGLE_CHROME" = "1";
-        };
-      };
-
-      containers.tika = {
-        image = "ghcr.io/paperless-ngx/tika:latest";
-      };
-    };
     modules.services.reverseProxy.proxies.paperless = {
       publicPort = publicPort;
       auth = false;
