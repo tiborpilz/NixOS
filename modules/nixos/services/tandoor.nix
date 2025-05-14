3db6b5d322dc2e9e5d96061fa62b3b73ca29b45e
Tibor Pilz <tibor.pilz@iu.org>
2025-05-14 10:54:21 +0200

    feat(tandoor): move to quadlet-nix for defining podman pods

--- modules/nixos/services/tandoor.nix
+++ modules/nixos/services/tandoor.nix
@@ -2,7 +2,7 @@
 with lib;
 with lib.my;
 let
-  publicPort = 8286;
+  publicPort = 8285;
   db_user = "recipes";
   db_password = "recipes";
   db_db = "recipes";
@@ -22,44 +22,66 @@ in
     system.activationScripts.backupTandoor = stringAfter [ "var" ] ''
       if ${pkgs.podman}/bin/podman volume exists tandoor-pgdata; then
         mkdir -p /data/backups
-        version=$(echo "${config.modules.podgroups.pods.tandoor.containers.tandoor.image}" | rev | cut -d"/" -f1 | rev)
-        backup_name="tandoor-pgdata-$version-$(date +%Y-%m-%d_%H%M%S).tar"
-        ${pkgs.podman}/bin/podman volume export tandoor-pgdata -o /data/backups/$backup_name
+        version=$(echo "${config.virtualisation.quadlet.containers.tandoor.containerConfig.image}" | rev | cut -d"/" -f1 | rev)
+        backup_suffix="$version-$(date +%Y-%m-%d_%H%M%S)"
+        db_backup_name="tandoor-pgdata-$version-$(date +%Y-%m-%d_%H%M%S).tar"
+        staticfiles_backup_name="tandoor-staticfiles-$version-$(date +%Y-%m-%d_%H%M%S)"
+        mediafiles_backup_name="tandoor-mediafiles-$version-$(date +%Y-%m-%d_%H%M%S)"
+
+        ${pkgs.podman}/bin/podman volume export tandoor-pgdata -o /data/backups/$db_backup_name
+        cp -r /var/lib/tandoor/staticfiles /data/backups/$staticfiles_backup_name
+        cp -r /var/lib/tandoor/mediafiles /data/backups/$mediafiles_backup_name
       fi
-    '';
+      '';
 
-    modules.podgroups.pods.tandoor = {
-      port = "${toString publicPort}:8080";
+    virtualisation.quadlet =
+      let
+        inherit (config.virtualisation.quadlet) network pods;
+      in
+      {
+        containers = {
+          tandoor-db.containerConfig = {
+            image = "docker.io/postgres:13";
+            volumes = [
+              "${pg_data}:/var/lib/postgresql/data"
+            ];
+            environments = {
+              POSTGRES_USER = db_user;
+              POSTGRES_PASSWORD = db_password;
+              POSTGRES_DB = db_db;
+            };
+            pod = pods.tandoor-pod.ref;
+          };
 
-      containers.db = {
-        image = "docker.io/postgres:13";
-        volumes = [ "${pg_data}:/var/lib/postgresql/data" ];
-        environment = {
-          "POSTGRES_DB" = db_db;
-          "POSTGRES_USER" = db_user;
-          "POSTGRES_PASSWORD" = db_password;
+          tandoor.containerConfig = {
+            image = "docker.io/vabene1111/recipes:1.5.27";
+            volumes = [
+              "/var/lib/tandoor/staticfiles:/opt/recipes/staticfiles"
+              "/var/lib/tandoor/mediafiles:/opt/recipes/mediafiles"
+            ];
+            environments = {
+              SECRET_KEY = "secretkey";
+              DB_ENGINE = "django.db.backends.postgresql";
+              POSTGRES_HOST = "localhost";
+              POSTGRES_PORT = "5432";
+              POSTGRES_USER = db_user;
+              POSTGRES_PASSWORD = db_password;
+              POSTGRES_DB = db_db;
+              ALLOWED_HOSTS = "*";
+              CSRF_TRUSTED_ORIGINS =
+                "https://tandoor.${config.modules.services.reverseProxy.hostname}";
+            };
+            pod = pods.tandoor-pod.ref;
+          };
+        };
+
+        pods.tandoor-pod.podConfig = {
+          publishPorts = [
+            "${toString publicPort}:8080"
+          ];
         };
       };
 
-      containers.tandoor = {
-        image = "docker.io/vabene1111/recipes:1.5.27";
-        environment = {
-          "SECRET_KEY" = "secretkey";
-          "DB_ENGINE" = "django.db.backends.postgresql";
-          "POSTGRES_HOST" = "localhost";
-          "POSTGRES_PORT" = "5432";
-          "POSTGRES_USER" = db_user;
-          "POSTGRES_PASSWORD" = db_password;
-          "POSTGRES_DB" = db_db;
-          "ALLOWED_HOSTS" = "*";
-          "CSRF_TRUSTED_ORIGINS" = "https://tandoor.${config.modules.services.reverseProxy.hostname}";
-        };
-        volumes = [
-          "tandoor-staticfiles:/opt/recipes/staticfiles"
-          "/var/lib/tandoor/mediafiles:/opt/recipes/mediafiles"
-        ];
-      };
-    };
     modules.services.reverseProxy.proxies.tandoor = {
       publicPort = publicPort;
       auth = false;
