c58a0101afc4cf06c564ee31b5a27277e183b0a4
Tibor Pilz <tibor.pilz@iu.org>
2025-05-20 19:17:11 +0200

    fix(nvim): restore old fold text

--- home/config/neovim/lua/plugins/fold.lua
+++ home/config/neovim/lua/plugins/fold.lua
@@ -1,57 +1,8 @@
--- Fold using Treesitter
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
 vim.opt.foldlevel = 99
-
-local handler = function(virtText, lnum, endLnum, width, truncate)
-    local newVirtText = {}
-    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-    local sufWidth = vim.fn.strdisplaywidth(suffix)
-    local targetWidth = width - sufWidth
-    local curWidth = 0
-    for _, chunk in ipairs(virtText) do
-        local chunkText = chunk[1]
-        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-        else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, {chunkText, hlGroup})
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-        end
-        curWidth = curWidth + chunkWidth
-    end
-    table.insert(newVirtText, {suffix, 'MoreMsg'})
-    return newVirtText
-end
-
+vim.opt.foldtext = ""
 
 return {
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = { 'kevinhwang91/promise-async' },
-    config = function()
-      require('ufo').setup({
-        fold_text_virt_handler = handler,
-      })
-    end,
-  },
-  -- {
-  --   "anuvyklack/pretty-fold.nvim",
-  --   config = function()
-  --     require("pretty-fold").setup({
-  --       sections = {
-  --         left = {
-  --           'content',
-  --         },
-  --       },
-  --     })
-  --   end
-  -- },
+  -- TODO: plugin or something for better fold fill text
 }
