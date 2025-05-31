60742db3658a734611c6722d8a6782ffd636970a
Tibor Pilz <tibor.pilz@iu.org>
2025-05-31 07:09:18 +0200

    feat(nvim): ui changes

--- home/config/neovim/lua/plugins/fold.lua
+++ home/config/neovim/lua/plugins/fold.lua
@@ -1,8 +1,25 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
 vim.opt.foldlevel = 99
-vim.opt.foldtext = ""
+vim.opt.foldtext = "x"
+vim.o.foldcolumn = '1'
 
 return {
-  -- TODO: plugin or something for better fold fill text
+  -- 'kevinhwang91/nvim-ufo',
+  -- dependencies = {
+  --   'kevinhwang91/promise-async',
+  -- },
+  -- setup = function()
+  --   vim.o.foldcolumn = '1' -- '0' is not bad
+  --   vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+  --   vim.o.foldlevelstart = 99
+  --   vim.o.foldenable = true
+  --
+  --   -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+  --   vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+  --   vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+  --
+  --   -- Option 1: coc.nvim as LSP client
+  --   require('ufo').setup()
+  -- end
 }
--- home/config/neovim/lua/plugins/ui.lua
+++ home/config/neovim/lua/plugins/ui.lua
@@ -29,49 +29,49 @@ return {
     end,
   },
   -- Status Column
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add          = { text = '┃' },
-          change       = { text = '┃' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked    = { text = '┆' },
-        },
-        preview_config = {
-          border = { " ", " ", " ", " ", " ", " ", " ", " " },
-        },
-      })
-
-      vim.keymap.set("n", "<leader>gB", "<cmd>Gitsigns blame_line<cr>", { desc = "Blame Line" })
-    end,
-  },
+  -- {
+  --   "lewis6991/gitsigns.nvim",
+  --   config = function()
+  --     require("gitsigns").setup({
+  --       signs = {
+  --         add          = { text = '┃' },
+  --         change       = { text = '┃' },
+  --         delete       = { text = '_' },
+  --         topdelete    = { text = '‾' },
+  --         changedelete = { text = '~' },
+  --         untracked    = { text = '┆' },
+  --       },
+  --       preview_config = {
+  --         border = { " ", " ", " ", " ", " ", " ", " ", " " },
+  --       },
+  --     })
+  --
+  --     vim.keymap.set("n", "<leader>gB", "<cmd>Gitsigns blame_line<cr>", { desc = "Blame Line" })
+  --   end,
+  -- },
   {
     "luukvbaal/statuscol.nvim",
-    config = function()
-      -- local builtin = require("statuscol.builtin")
-      require("statuscol").setup({
-        -- configuration goes here, for example:
-        -- relculright = true,
-        -- segments = {
-        --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-        --   {
-        --     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-        --     click = "v:lua.ScSa"
-        --   },
-        --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-        --   {
-        --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-        --     click = "v:lua.ScSa"
-        --   },
-        -- }
-      })
-    end,
+    dependencies = {
+      "fussenegger/nvim-dap",
+    },
+      opts = function()
+        local builtin = require('statuscol.builtin')
+        return {
+          setopt = true,
+          -- override the default list of segments with:
+          -- number-less fold indicator, then signs, then line number & separator
+          segments = {
+            { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+            { text = { '%s' }, click = 'v:lua.ScSa' },
+            {
+              text = { builtin.lnumfunc, ' ' },
+              condition = { true, builtin.not_empty },
+              click = 'v:lua.ScLa',
+            },
+          },
+        }
+      end,
   },
-
 
   -- Zen Mode
   {
