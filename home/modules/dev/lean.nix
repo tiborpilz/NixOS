b111f292782586b5d65a2960ef7588e0795998ff
Tibor Pilz <tibor@pilz.berlin>
2025-08-09 21:30:03 +0200

    feat(jj): add jujutsu

---
+++ home/modules/dev/lean.nix
@@ -0,0 +1,16 @@
+{ inputs, config, lib, pkgs, ... }:
+let
+  cfg = config.modules.dev.lean;
+  mylib = import ../../../lib { inherit inputs lib pkgs; };
+in
+{
+  options.modules.dev.lean = {
+    enable = mylib.mkBoolOpt false;
+  };
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      lean4
+      elan
+    ];
+  };
+}
