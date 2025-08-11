5e41f9dc8a02c29e5c754b7161973f147b1cbb7b
Tibor Pilz <tibor.pilz@iu.org>
2025-08-11 16:18:00 +0200

    feat(nvim): add twighlight

---
+++ home/config/neovim/lua/plugins/twilight.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/twilight.nvim",
+  opts = {
+    context = 20,
+    treesitter = true,
+    expand = {
+      "function",
+      "method",
+      "table",
+      "if_statement",
+      "for_statement",
+      "while_statement",
+      "repeat_statement",
+      "switch_statement",
+    },
+  },
+}
