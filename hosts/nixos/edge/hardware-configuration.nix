801e53e9ed738e78c92fc0f3b3296f237174c23f
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 10:17:38 +0200

    fix(edge): remove double boot definition

--- hosts/nixos/edge/hardware-configuration.nix
+++ hosts/nixos/edge/hardware-configuration.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "nodev";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
 }
