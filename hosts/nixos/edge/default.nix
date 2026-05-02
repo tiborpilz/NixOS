2413d2012463b3ae36b8bd359af73b600defa648
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 14:03:51 +0200

    fix: get hetzner edge vm working

--- hosts/nixos/edge/default.nix
+++ hosts/nixos/edge/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, inputs, ... }: {
   imports = [
     ./hardware-configuration.nix
     ./disko.nix
@@ -26,6 +26,8 @@
   users.users.tibor.isNormalUser = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEyxiA698x3QgDpUhjGVXo5gtx4oApzYp5RmNleZrsMxGyT89zoGZU1Wi0xlSUF+R0LrTwbOdT+jC4Ym2Ebf497by/w5StxBmiiqHkpq4kGIb8dPP39ez9XaweH1Xi7G3UpNzaJvblDD6jCGWG0zV2CWuHuhRu8O4zRY+63qoh8gKd3aiDRrSJSPv2U1HwIs1ppk3rj7gPIFX/3lmwNo2LA2KKPAvYR1qTmkCnsEEkTzi2zIzztBoHCWSRHtv1374zF/L5EmV+EScL1BEK667kUUDbNAMXVL3juR/Hb/LRwjiO053rhj1NQ+jy2uH1UYwkdg7UM7N4uPKjFtPsC6oPNLCRCH0XpX9vp8U6T4GDz3ypVRzq1zfiRIYl5X+EgqgKw5eHEn/44VWSt/lKv5THcMmb3cMRTfYjUALNhp8XWF2/FsbS02Da7qKGLfUK7kreE+PPJYk3rAWpU+gMaeXU+ILz+sLrD2Lrkkeuq7PocxLAraz3KslO48xXNnM8sc8= tibor@workyMcWorkstation"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCe+KzPWNqzt+9/0EB0x92q0t1dCTHrq4W0Dn/qednCh9Ib8tsm5tbpI3hKJGoOvVfIr2dJe+pJA1mBua15jcShxNPVeB5WwpWMCm5BHB4JZCN5ZeiPvwQ60gB5fEmwq/JCVp1BznOLrC/acKE0elP6r1YD1CcgVd5whH2vibLmP8ahaqXsndUDCRSxoxD/10+8Uk3hLRYGIp8F28oiuumz9nqDhSDk9e/OXve7tASfvuos8t8d6/Hl10LSAPvpeLB6SqSaMn9PfleEMWrlurranB0/JTcT+gZJ6QfnGoKSAMOA3FCgDJ7WfQ/DClggL4xdvdT1SYfUip/sPaDPWP6IidnIOmhnZkpyYuEjUETq4MW5V0jEM5gXOhnvMYzfrM9hAn0ob1WPF/o6uUilmeDIuUb4O9Wj+xpgB1/WuO+4DQJEUXpX0mZTn6lDOiHRm1n+Yg/ZL9yfJsaUGMOPDsKl4QTo2Idaju+2/xtAR1V0LkPduaq8B2bPO5fu4aTaM18= tiborpilz@MacBook-Pro-MBP-L1670"
+
   ];
 
   programs.zsh.enable = true;
--- hosts/nixos/edge/hardware-configuration.nix
+++ hosts/nixos/edge/hardware-configuration.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.loader.grub.enable = true;
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
   boot.initrd.kernelModules = [ "nvme" ];
 }
