1ba8edf333c0d561d0ef278cbadbf37980074893
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 10:07:09 +0200

    fix(edge): fix dual mirroredBoots

--- hosts/nixos/edge/disko.nix
+++ hosts/nixos/edge/disko.nix
@@ -12,16 +12,6 @@
             type = "EF02"; # BIOS boot partition
           };
 
-          ESP = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-
           root = {
             size = "100%";
             content = {
