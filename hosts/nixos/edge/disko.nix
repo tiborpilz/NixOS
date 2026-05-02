5c889a6d38bbc50d26c219b0d4f4528fe74d1fc0
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 10:16:08 +0200

    fix: i like filesystems

--- hosts/nixos/edge/disko.nix
+++ hosts/nixos/edge/disko.nix
@@ -12,6 +12,16 @@
             type = "EF02"; # BIOS boot partition
           };
 
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
           root = {
             size = "100%";
             content = {
--- hosts/nixos/edge/hardware-configuration.nix
+++ hosts/nixos/edge/hardware-configuration.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
-  boot.loader.grub.device = "nodev";
+  boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
 }
