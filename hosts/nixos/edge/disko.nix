a348dfdc0f477b8cc2e5b8e4f546eb3beeecd028
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 14:03:51 +0200

    fix: get hetzner edge vm working

--- hosts/nixos/edge/disko.nix
+++ hosts/nixos/edge/disko.nix
@@ -1,33 +1,34 @@
-{ ... }:
+{ lib, ... }:
 {
   disko.devices = {
-    disk.main = {
-      type = "disk";
-      device = "/dev/sda";
-      content = {
-        type = "gpt";
-        partitions = {
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+              priority = 1;
             };
-          };
-
-          bios = {
-            size = "1M";
-            type = "EF02";
-          };
-
-          ESP = {
-            size = "244M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot/efi";
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
