d09fc229192bb943f7321325c2d99894dfcc1773
Tibor Pilz <tibor@pilz.berlin>
2026-05-02 10:27:53 +0200

    fix(edge): use lvm storage

--- hosts/nixos/edge/disko.nix
+++ hosts/nixos/edge/disko.nix
@@ -1,23 +1,51 @@
-{ ... }:
+{ lib, ... }:
 {
   disko.devices = {
-    disk.main = {
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/sda";
       type = "disk";
-      device = "/dev/sda";
       content = {
         type = "gpt";
         partitions = {
           boot = {
+            name = "boot";
             size = "1M";
             type = "EF02";
           };
-
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
           root = {
+            name = "root";
             size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
             };
           };
         };
@@ -25,4 +53,3 @@
     };
   };
 }
-
