415a45910458ac61b1d65aea2c7deb7cd7890615
Tibor Pilz <tibor@pilz.berlin>
2026-05-08 20:14:04 +0200

    fix(doom): actually working doom emacs as nix package

--- lib/flake.nix
+++ lib/flake.nix
@@ -1,5 +1,8 @@
 { lib, inputs, ... }:
 {
   mkApp = package:
-    inputs.flake-utils.lib.mkApp { drv = package; };
+    inputs.flake-utils.lib.mkApp {
+      drv = package;
+      exePath = "/bin/${package.meta.mainProgram or package.pname or package.name}";
+    };
 }
